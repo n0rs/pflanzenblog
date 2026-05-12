408ae70119fd4560f90a8ef40937efe1692e545d
lgr545 <livhanna.grimsehlschmitz@rfh-campus.de>
2026-05-12 14:16:31 +0200

    erstellen test

---
+++ erstellen.php
@@ -0,0 +1,28 @@
+<?php
+require_once 'db.php';
+/** @var PDO $pdo */
+
+if (isset($_POST['submit_post'])) {
+    $titel = $_POST['titel'];
+    $inhalt = $_POST['inhalt'];
+    $autor_id = $_POST['autor_id'];
+
+    $stmt = $pdo->prepare("INSERT INTO beitraege(titel, inhalt, autor_id) VALUES (?,?,?)");
+
+    if ($stmt->execute([$titel, $inhalt, $autor_id])) {
+        echo "Beitrag erstellt";
+        header("Location: index.php");
+        exit;
+    }
+
+}
+?>
+
+<form method="post" action="erstellen.php">
+    <input type="text" name="titel" id="titel" placeholder="Titel">
+    <label for="titel">Titel:</label>
+    <input type="text" name="inhalt" id="inhalt" placeholder="inhalt">
+    <label for ="inhalt">Inhalt:</label>
+    <input type="number" name="autor_id" id="autor_id" placeholder="autor_id">
+    <label for ="autor_id">Autorennummer:</label>
+</form>
