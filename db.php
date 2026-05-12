dcb372f65ce9477eef600856af6fac8ff6aa3821
lgr545 <livhanna.grimsehlschmitz@rfh-campus.de>
2026-05-12 13:35:43 +0200

    umbenannt in index php, db php

--- db.php
+++ db.php
@@ -8,8 +8,8 @@ $pass = 'TwwqC6a?Ossv!x55';
 $dsn = "mysql:host=$host;dbname=$db;charset=utf8mb4";
 
 $options = [
-        PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION, // Zeigt SQL-Fehler an
-        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,       // Gibt Daten als schönes Array zurück
+        PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
 ];
 
 try {
