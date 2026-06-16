d9256462e63a418f3b4b3202993c759d418e54dc
lgr545 <livhanna.grimsehlschmitz@rfh-campus.de>
2026-06-16 20:11:40 +0200

    hintergrundbild fix fix

--- index.php
+++ index.php
@@ -50,7 +50,7 @@ $beitraege = holeBeitraegeProSeite($datenbankverbindung, $beitraege_pro_seite, $
         <link rel="icon" type="image/svg+xml" href="<?php echo serverAssetPfad('icons', 'favicon.svg'); ?>">
         <link rel="stylesheet" href="stylesheet.css">
     </head>
-    <body style="background-image: url('<?php echo serverAssetPfad('bilder', 'natalie-kovach-ph7QQq63lCs-unsplash.jpg'); ?>');">
+    <body style="background-image: url('<?php echo serverAssetPfad('icons', 'natalie-kovach-ph7QQq63lCs-unsplash.jpg'); ?>');">
         <div class="container">
 
             <?php include 'kopfzeile.php'; ?>
